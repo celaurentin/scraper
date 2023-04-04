0aa14c5628f34da9e74de7efabcb4fa7dfe176ac
Cesar Laurentin <claurentin@gmail.com>
2023-04-04 17:58:47 -0500

    Scraping and printing out

--- main.py
+++ main.py
@@ -1,16 +1,29 @@
-# This is a sample Python script.
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
 
+# From the 500 Items and Services List table we only want the first column (CODE)
+# to print them in this format: ('code1','code2',...'codeN')
+url = "https://www.cms.gov/healthplan-price-transparency/resources/500-items-services"
+output = ""
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+options = Options()
+options.add_argument("--headless")
 
+driver = webdriver.Chrome(options=options, service=Service(ChromeDriverManager().install()))
+driver.get(url)
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+table = driver.find_element(By.TAG_NAME, "tbody")
+rows = table.find_elements(By.TAG_NAME, "tr")
+print(f"Scraping {len(rows)} codes...")
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+print("(",end="")
+for row in rows:       
+    col = row.find_elements(By.TAG_NAME, "td")[0]
+    output += "'"+col.text.strip()+"',"
+
+print(output[:-1]+")")
+driver.quit()
