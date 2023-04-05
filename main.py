de5863897a0f75b40f23f511d86316d188ca59c4
Cesar Laurentin <claurentin@gmail.com>
2023-04-05 13:52:46 -0500

    Adding csv support

--- main.py
+++ main.py
@@ -1,14 +1,28 @@
 
+import csv
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 
-# From the 500 Items and Services List table we only want the first column (CODE)
+
+# Problem #1: From the 500 Items and Services List table we only want the first column values (Code)
 # to print them in this format: ('code1','code2',...'codeN')
+
+# Problem #2: From the 500 Items and Services List table we want to store all column values (Code, Description
+# Plain Language Description) into a data structure, and then generate a csv file from there.
+
+class item:
+    def __init__(self, code, description, plainDescription):
+        self.code = code
+        self.description = description
+        self.plainDescription = plainDescription
+
+items = []
+
 url = "https://www.cms.gov/healthplan-price-transparency/resources/500-items-services"
-output = ""
+outputSol1 = ""
 
 options = Options()
 options.add_argument("--headless")
@@ -19,11 +33,22 @@ driver.get(url)
 table = driver.find_element(By.TAG_NAME, "tbody")
 rows = table.find_elements(By.TAG_NAME, "tr")
 print(f"Scraping {len(rows)} codes...")
-
+     
+#Problem 1 solution
 print("(",end="")
 for row in rows:       
-    col = row.find_elements(By.TAG_NAME, "td")[0]
-    output += "'"+col.text.strip()+"',"
+    code = row.find_elements(By.TAG_NAME, "td")[0].text.strip()
+    description = row.find_elements(By.TAG_NAME, "td")[1].text.strip().replace(",","").replace("\n","")
+    plainDescription = row.find_elements(By.TAG_NAME, "td")[2].text.strip().replace(",","")
+    outputSol1 += "'"+code+"',"
+    items.append(item(code, description, plainDescription))
+print(outputSol1[:-1]+")")
 
-print(output[:-1]+")")
+#Problem 2 solution
+with open('500items.csv', 'w', newline='') as file:
+     writer = csv.writer(file)
+     writer.writerow(["Code", "Description", "Plain Language Description"])
+     for record in items:
+        writer.writerow([record.code, record.description, record.plainDescription])
+file.close
 driver.quit()
